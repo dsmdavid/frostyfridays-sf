0f00cee453caea9fcba968bc6c7c5c5a164a4aef
dsmdavidIL <61737063+dsmdavidIL@users.noreply.github.com>
2022-09-16 12:57:39 +0100

    add internal array

--- models/challenge_14_02.sql
+++ models/challenge_14_02.sql
@@ -3,7 +3,19 @@
     materialized = 'view',
     )
 }}
-
+with base as (
+  select * from {{ ref('challenge_14_01') }}
+),
+superpowers as (
+   select 
+    country_of_residence,
+    superhero_name,
+    notable_exploits,
+    array_construct_compact( superpower,
+    second_superpower,
+    third_superpower) as superpowers
+   from base
+)
 select
   object_construct_keep_null(*) as superhero_json
-from {{ ref('challenge_14_01') }}
+from superpowers
