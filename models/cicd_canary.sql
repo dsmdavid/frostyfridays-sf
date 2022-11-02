717176abdd8723fa909b0606719ca46608252300
dsmdavidIL <61737063+dsmdavidIL@users.noreply.github.com>
2022-11-02 09:54:30 +0000

    lint files previously failing

--- models/cicd_canary.sql
+++ models/cicd_canary.sql
@@ -4,7 +4,12 @@
     )
 }}
 
-select 
-    e_data , -- s
+WITH prep_1 AS (SELECT
+    'user@domain' AS e_data,
+    1 AS n_counter
+)
+
+SELECT
+    e_data, -- s
     n_counter
-from (select 'user@domain' as e_data, 1 as n_counter)
+FROM prep_1
