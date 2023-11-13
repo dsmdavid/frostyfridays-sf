52e92382b2ab2fe91c007f1918991c5af75fda56
dsmdavid <root@localhost>
2023-11-13 12:01:27 +0000

    doc: remove doc

--- models/challenge_71_01_sales.sql
+++ models/challenge_71_01_sales.sql
@@ -4,17 +4,6 @@
     )
 }}
 
-
--- Create the Sales table
-{# CREATE OR REPLACE TABLE Sales (
-    Sale_ID INT PRIMARY KEY,
-    Product_IDs VARIANT --INT
-);
-
--- Inserting sample sales data
-INSERT INTO Sales (Sale_ID, Product_IDs) SELECT 1, PARSE_JSON('[1, 3]');-- Products A and C in the same sale
-INSERT INTO Sales (Sale_ID, Product_IDs) SELECT 2, PARSE_JSON('[2, 4]');-- Products B and D in the same sale #}
-
 SELECT
     1 AS sale_id,
     PARSE_JSON('[1,3]') AS product_ids
