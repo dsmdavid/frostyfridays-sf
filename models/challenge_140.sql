22f1b3d37f98255256792e3bd926f6f99697b34f
David Sanchez <david.sanchez@datarsys.co.uk>
2025-07-06 23:20:29 +0100

    chore: apply linting

--- models/challenge_140.sql
+++ models/challenge_140.sql
@@ -3,30 +3,47 @@
     materialized = 'table'
  )
 }}
-{# macro init_challenge_140 used as run-operation to create the required tables and sample rows #}
+{# macro init_challenge_140 used as run-operation 
+to create the required tables and sample rows #}
 
 {% if var('ch140', var('run_all', false)) %}
-    with categories as (
-        select array_agg(category_name) as categories from {{ target.database }}.{{ target.schema }}.ch140_service_categories
-    ),
-    line_items as (
-        select * from {{ target.database }}.{{ target.schema }}.ch140_invoice_line_items
-    ),
-    combined as (
-        select * 
-        from line_items
-        cross join categories
-    ),
-    classify_text as (
-        select 
-            line_item_id,
-            service_description,
-            categories,
-            SNOWFLAKE.CORTEX.CLASSIFY_TEXT( service_description , categories)['label']::varchar as classification
+    WITH
+        categories AS (
+            SELECT array_agg(category_name) AS categories
+            FROM
+                {{ target.database }}.{{ target.schema }}.ch140_service_categories -- noqa: LT05
+        ),
 
-        from combined
-    )
-    select * from classify_text
+        line_items AS (
+            SELECT *
+            FROM
+                {{ target.database }}.{{ target.schema }}.ch140_invoice_line_items -- noqa: LT05
+        ),
+
+        combined AS (
+            SELECT
+                line_items.*,
+                categories.*
+            FROM line_items
+                CROSS JOIN categories
+        ),
+
+        classify_text AS (
+            SELECT
+                line_item_id,
+                service_description,
+                categories,
+                SNOWFLAKE.CORTEX.CLASSIFY_TEXT(
+                    service_description,
+                    categories)['label']::varchar
+                    AS classification
+
+            FROM combined
+        )
+
+    SELECT * FROM classify_text
 {% else %}
-    SELECT 'raw_data not created, use var ch140 to enable it.' AS output_
+    SELECT
+        'raw_data not created, use var ch140 to enable it.'
+            AS output_
 {% endif %}
